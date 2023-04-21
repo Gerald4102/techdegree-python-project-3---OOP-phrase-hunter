313641d6bb2687059010329c5a624989e47ca83f
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-04-21 23:42:50 +0100

    draft

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,63 @@
-# Create your Game class logic in here.
+import random
+
+from phrasehunter.phrase import Phrase
+
+
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.lives = 5
+        self.phrases = [
+            Phrase("Frankly my dear I dont give a damn"), 
+            Phrase("Im going to make him an offer he cant refuse"), 
+            Phrase("Toto Ive got a feeling we are not in Kansas anymore"), 
+            Phrase("Go ahead make my day"), 
+            Phrase("May the Force be with you")
+            ]
+        self.active_phrase = None
+        self.guesses = []
+
+    def start(self):
+        self.active_phrase = self.get_random_phrase()
+        self.active_phrase_list = list(self.active_phrase.phrase)
+        self.welcome()
+        Phrase.display(self)
+        while True:
+            guess = self.get_guess()
+            Phrase.check_letter(self,guess)
+            self.game_over()
+
+    def get_random_phrase(self):
+        return random.choice(self.phrases)
+
+    def welcome(self):
+        print('\n')
+        print('Welcome to PHRASE HUNTER \n')
+
+    def get_guess(self):
+        return input('Guess a letter: ').lower()
+
+    def game_over(self):
+        phrase = set(self.active_phrase.phrase) - set(' ')
+        guesses = set(self.guesses)
+        if phrase.issubset(guesses):
+            print('You won! \n')
+            self.restart()
+        elif self.missed == self.lives:
+            print('GAME OVER')
+            self.restart()
+
+    def restart(self):
+        restart = input('Would you like to play again? Y/N \n').lower()
+        if restart == 'y':
+            Game().start()
+        elif restart == 'n':
+            print('Thank you for playing. \n')
+            exit()
+        else:
+            self.restart()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,36 @@
-# Create your Phrase class logic here.
+class Phrase:
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+
+    def display(self):
+        display_list = []
+        for letter in self.active_phrase_list:
+            if letter == ' ':
+                display_list.append(' ')
+            elif letter in self.guesses:
+                display_list.append(letter)
+            else:
+                display_list.append('_')
+        display = ''.join(display_list)
+        print(display, '\n')
+
+    def check_letter(self, guess):
+        if len(list(guess)) != 1 or guess == ' ':
+            print(f'"{guess}" is not a valid guess')
+        elif guess not in 'qwertyuiopasdfghjklzxcvbnm':
+            print(f'{guess} is not a letter!')
+        elif guess in self.guesses:
+            print(f'You have already tried {guess} \n')
+        elif guess in self.active_phrase.phrase:
+            self.guesses.append(guess)
+            print('Good guess! \n')
+            Phrase.display(self)
+        else:
+            print(f'{guess} is not in the phrase \n')
+            self.missed += 1
+            print(f'You have {self.lives - self.missed} out of {self.lives} lives remaining! \n')
+            self.guesses.append(guess)
+
+    def check_complete(self):
+        pass
+
